61713e7705e509094a885679bffe87ca30787b7f
362707242 <362707242@2260I-R31-001>
2026-04-17 13:13:57 -0400

    Add project files.

---
+++ animation/Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace animation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D tribbleBTexture, tribbleCTexture, tribbleGTexture, tribbleOTexture;
+
+        Rectangle window, tribbleBRect;
+
+        Vector2 tribbleBSpeed;
+
+        SoundEffect tribbleSound, whistle;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            window = new Rectangle(0,0,800,600);
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.ApplyChanges();
+
+            tribbleBRect = new Rectangle(0, 0, 100, 100);
+            tribbleBSpeed = new Vector2(6, 8);
+            
+            
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            tribbleBTexture = Content.Load<Texture2D>("tribbleBrown");
+            tribbleSound = Content.Load<SoundEffect>("tribble_coo");
+            whistle = Content.Load<SoundEffect>("mixkit-cartoon-toy-whistle-616");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            tribbleBRect.X += (int)tribbleBSpeed.X;
+            if (tribbleBRect.Right > window.Width || tribbleBRect.Left <= 0)
+            {
+                tribbleBSpeed.X *= -1;
+                tribbleSound.Play();
+               
+            }
+
+
+
+            tribbleBRect.Y += (int)tribbleBSpeed.Y;
+            if (tribbleBRect.Top <= 0 || tribbleBRect.Bottom > window.Height)
+            {
+                tribbleBSpeed.Y *= -1;
+                whistle.Play();
+
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(tribbleBTexture, tribbleBRect, Color.White);
+
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ animation/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new animation.Game1();
+game.Run();
