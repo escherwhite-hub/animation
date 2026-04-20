d5f8b09cd45d8e5e41ba7c614b3a693310c16ba1
362707242 <362707242@2260I-R31-001>
2026-04-20 12:56:46 -0400

    class done

--- animation/Game1.cs
+++ animation/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace animation
 {
@@ -12,11 +13,14 @@ namespace animation
 
         Texture2D tribbleBTexture, tribbleCTexture, tribbleGTexture, tribbleOTexture;
 
-        Rectangle window, tribbleBRect;
+        Rectangle window, tribbleBRect, tribbleCRect, tribbleGRect;
 
-        Vector2 tribbleBSpeed;
+        Vector2 tribbleBSpeed, tribbleCSpeed, tribbleGSpeed;
 
-        SoundEffect tribbleSound, whistle;
+        SoundEffect tribbleSound;
+
+        Random random;
+        
 
         public Game1()
         {
@@ -34,8 +38,13 @@ namespace animation
 
             tribbleBRect = new Rectangle(0, 0, 100, 100);
             tribbleBSpeed = new Vector2(6, 8);
-            
-            
+            tribbleCRect = new Rectangle(5,0, 100, 100);
+            tribbleCSpeed = new Vector2(4, 1);
+            tribbleGRect = new Rectangle(0, 0, 100, 100);
+            tribbleGSpeed = new Vector2(4, 1);
+
+
+
             // TODO: Add your initialization logic here
 
             base.Initialize();
@@ -46,8 +55,10 @@ namespace animation
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             tribbleBTexture = Content.Load<Texture2D>("tribbleBrown");
+            tribbleCTexture = Content.Load<Texture2D>("tribbleCream");
+            tribbleGTexture = Content.Load<Texture2D>("tribbleGrey");
             tribbleSound = Content.Load<SoundEffect>("tribble_coo");
-            whistle = Content.Load<SoundEffect>("mixkit-cartoon-toy-whistle-616");
+            
             // TODO: use this.Content to load your game content here
         }
 
@@ -55,6 +66,8 @@ namespace animation
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            
+            random = new Random();
 
             // TODO: Add your update logic here
             tribbleBRect.X += (int)tribbleBSpeed.X;
@@ -71,9 +84,39 @@ namespace animation
             if (tribbleBRect.Top <= 0 || tribbleBRect.Bottom > window.Height)
             {
                 tribbleBSpeed.Y *= -1;
-                whistle.Play();
+                tribbleSound.Play();
 
             }
+
+            tribbleCRect.X += (int)tribbleCSpeed.X;
+            if (tribbleCRect.Right > window.Width || tribbleCRect.Left <= 0)
+            {
+                tribbleCRect.X = random.Next(0,700);
+            }
+
+            tribbleCRect.Y += (int)tribbleCSpeed.Y;
+            if (tribbleCRect.Bottom > window.Height || tribbleCRect.Top <= 0)
+            {
+                tribbleCRect.Y = random.Next(0,500);
+            }
+
+
+
+            tribbleGRect.X += (int)tribbleGSpeed.X;
+            if (tribbleGRect.Right > window.Width || tribbleGRect.Left <= 0)
+            {
+                tribbleGSpeed.X = 1;
+                tribbleGSpeed.X *= random.Next(-10, -1);
+            }
+
+            tribbleGRect.Y += (int)tribbleGSpeed.Y;
+            if (tribbleGRect.Bottom > window.Height || tribbleGRect.Top <= 0)
+            {
+                tribbleGSpeed.X = 1;
+                tribbleGSpeed.Y *= random.Next(-10, -1);
+            }
+
+           
 
             base.Update(gameTime);
         }
@@ -85,6 +128,8 @@ namespace animation
             _spriteBatch.Begin();
 
             _spriteBatch.Draw(tribbleBTexture, tribbleBRect, Color.White);
+            _spriteBatch.Draw(tribbleCTexture, tribbleCRect, Color.White);
+            _spriteBatch.Draw(tribbleGTexture, tribbleGRect, Color.White);
 
             _spriteBatch.End();
             // TODO: Add your drawing code here
